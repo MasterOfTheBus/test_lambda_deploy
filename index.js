16db11974d10aaa9655c0b1eeae45d1b6e4fbd6f
Sidney Ng <yendisng@gmail.com>
2022-01-02 17:56:16 -0500

    make a change to the handler so that path is returned

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  const method = event.httpMethod;
+  if (method === 'GET') {
+    if (event.path === '/') {
+      const response = {
+          statusCode: 200,
+          body: JSON.stringify('Hello from Lambda root!'),
+      };
+      return response;
+    } else if (event.path === '/hello') {
+      const response = {
+          statusCode: 200,
+          body: JSON.stringify(`Hello from Lambda! Path is ${event.path}`),
+      };
+      return response;
+    } else {
+      const response = {
+          statusCode: 500,
+          body: JSON.stringify(`Sorry, path ${event.path} not known.``),
+      };
+      return response;
+    }
+  }
 };
